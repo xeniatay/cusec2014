5df0b6be392c4e1960beddf3e88b3ebeb30a0373
Xenia Tay <xeniatzy@gmail.com>
2013-11-28 16:47:17 -0500

    Good and bad things happening here. Don't look.

---
+++ page-talks.php
@@ -0,0 +1,56 @@
+
+
+Gary Bernhardt
+Abstract: "The Birth & Death of JavaScript"
+
+This talk traces JavaScript's impact on software development tools over
+forty years, from 1995 until 2035. Although the language is mostly dead
+today, it drove the largest transformation of mainstream development tools
+since the creation of Unix 65 years ago.
+
+We'll examine some key moments in JavaScript's history: its accidental rise
+to popularity; asm.js, allowing arbitrary software to run at native speed
+in browsers; the language ecosystem that grew up around asm.js, displacing
+JavaScript itself; the replacement of traditional Unix terminals, shells,
+and editors with new, higher-level equivalents running against the DOM; and
+finally, the inversion of the browser-OS relationship, still ongoing.
+
+This story is told by someone who was a fervent JavaScript detractor during
+its rise. It was easy to see the language's numerous and glaring
+shortcomings, but miss the path that it laid out.
+
+
+Josh Matthews
+"This tutorial will provide a practical introduction to contributing code to Firefox. Learn how to find tasks to work on, teams to join, and tricks for navigating the 10 million lines of C++/JavaScript. We'll also walk through practical worked examples, such as adding a new DOM API for websites to use, and moving I/O onto a background thread."
+
+
+Steve Sisalith
+Abstract: “What is Design Thinking?”
+Creating innovative solutions and products that deliver value for users is rooted in the ability to understand user needs and what will enable them to reach their goals. However, we often forget that users are human, a complex mixture of logic and emotions. Successful products and services appeal to both the rational side as well as the emotional side of human intelligence. Design Thinking provides an approach for balancing these needs by combining traditional business and technological understanding with insights into human behaviour.
+
+
+
+
+Keynotes
+Gary Bernhardt
+Zach Holman
+Michelle Zatlyn
+Don Olmstead
+Software Engineer, Sony Network Entertainment International
+
+
+Non-Keynotes
+Bruno Morency
+David Mirza
+Dr. Derek Ruths
+Ian Chan
+Josh Matthews
+Maria Smirnoff
+Nadim Kobeissi
+Steve Sisalith
+Julia Evans
+Carlos Sanchez
+
+
+
+Non-Keynotes
