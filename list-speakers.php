13601ba6f47662c70fef3df1c7ba41cedcaa83a2
Xenia Tay <xeniatzy@gmail.com>
2013-12-05 00:51:24 -0500

    A big bunch of speaker stuff

---
+++ list-speakers.php
@@ -0,0 +1,118 @@
+<?php
+/* Pardon the hardcoded PHP.
+ * Guilty as charged. No excuses. :(
+ * Plz judge this book by its cover instead
+*/
+
+$speaker_tiers = array(
+    array(
+        'tier' => 'keynote',
+        'speakers' => array(
+            array(
+                'name' => 'Gary Bernhardt',
+                'title' => 'Creator & Destroyer of Software, Destroy All Software LLC',
+                'link' => '',
+                'img' => 'gary-bernhardt.png',
+                'desc' => "Gary Bernhardt is a creator and destroyer of software compelled to understand both sides of heated software debates: Vim and Emacs; Python and Ruby; Git and Mercurial. He runs Destroy All Software, which publishes advanced screencasts for serious developers covering Unix, Ruby, OO design, and TDD."
+            ),
+            array(
+                'name' => 'Zach Holman',
+                'title' => 'Engineer, GitHub',
+                'link' => '',
+                'img' => 'zach-holman.png',
+                'desc' => "Zach joined GitHub in 2010 as one of their first engineering hires. Initially working on what would become GitHub Enterprise, he now hacks on new features and frequently gives talks about building products and growing startups. He lives in San Francisco."
+            ),
+            array(
+                'name' => 'Michelle Zatlyn',
+                'title' => 'Co-founder & Head of User Experience, CloudFlare Inc.',
+                'link' => '',
+                'img' => 'michelle-zatlyn.png',
+                'desc' => "Michelle Zatlyn is the co-founder & Head of User Experience of CloudFlare, the web performance and security company. <br/>Michelle creates products people love. Prior to CloudFlare, she worked at Google and Toshiba and launched two successful startups. In 2012, CloudFlare was selected by the Wall Street Journal as the Most Innovative Internet Technology company, and named a Technology Pioneer by the World Economic Forum. Today CloudFlare accelerates and protects more than 180 billion page views for more than 800 million unique visitors every month. <br/>Michelle holds a Bachelor of Science degree in Chemistry and a minor in Management, with Distinction, from McGill University. She also holds her MBA from Harvard Business School and was awarded the Dubliner Prize for Entrepreneurship. Michelle is an experienced public speaker, frequent panespeakers and sits on the Open Internet Advisory Committee for the FCC. She was named one of the Top Women to Watch in Technology by the Huffington Post and one of the Top 15 Women to Watch in Tech by Inc. Magazine."
+            ),
+            array(
+                'name' => 'Don Olmstead',
+                'title' => 'Software Engineer, Sony Network Entertainment International',
+                'link' => '',
+                'img' => '',
+                'desc' => ""
+            ),
+        ),
+    ),
+    array(
+        'tier' => 'non-keynote',
+        'speakers' => array(
+            array(
+                'name' => 'Bruno Morency',
+                'title' => 'Founder, Context.IO',
+                'link' => '',
+                'img' => 'bruno-morency.png',
+                'desc' => ""
+            ),
+            array(
+                'name' => 'David Mirza',
+                'title' => 'Founder, Subgraph',
+                'link' => '',
+                'img' => 'david-mirza.png',
+                'desc' => "David is one of the founders of Subgraph, a Montreal-based open source security startup. Prior to this, he was a founding member of Security Focus, another security startup which was acquired by Symantec in 2002. While at Security Focus, David moderated the Bugtraq mailing speakers, a historically important forum for discussion of security vulnerabilities, for over four years. He has spoken at Black Hat, Can Sec West, AusCERT, OSCON, FISL and numerous other security and open source conferences, as well as made contributions to books, magazines and other publications. David also participated in a NIAC working group on behalf of Symantec to develop the first version of the CVSS (Common Vulnerability Scoring System) model and served as editor for the Attack Trends section of IEEE Security & Privacy for over three years. At any given moment he is likely working on something related to computer security, cloud computing, or privacy/free speech online."
+            ),
+            array(
+                'name' => 'Dr. Derek Ruths',
+                'title' => 'Assistant Professor, Network Dynamics Lab, McGill University',
+                'link' => '',
+                'img' => '',
+                'desc' => "Derek Ruths is an assistant professor of Computer Science at McGill University.  He joined the faculty in 2009 after completing his PhD in Computer Science at Rice University.  A major research direction in his group considers the problem of characterizing and predicting the large-scale dynamics of human behavior in online social platforms.  His ongoing work in this area includes quantitatively modeling how communities change over time, measuring and predicting group demographics from unstructured user-generated content, and computational methods for assessing discussion topics within a collection of users.  His work has been published in top-tier journals and conferences including Science, ICWSM, and PLoS Computational Biology.  His research is currently funded by a wide array of organizations including NSERC, SSHRC, tech companies, and the US National Science Foundation - underscoring the broad, interdisciplinary nature of his work."
+            ),
+            array(
+                'name' => 'Ian Chan',
+                'title' => 'Engineer, Twitter',
+                'link' => '',
+                'img' => 'ian-chan.png',
+                'desc' => "Ian Chan is a Staff Engineer at Twitter, where he's worked on the Platform and Web team for the last 3 years. He has a passion for startups, technology, platforms and consumer product development. Prior to moving to San Francisco, he worked in Toronto as a software developer for Big Corporation (read: IBM) and then a hot tech startup (read: tiny defunct startup you've never heard of). Ian received a B.Sc in Computer Science from the University of Toronto where he was a regular attendee of CUSEC."
+            ),
+            array(
+                'name' => 'Josh Matthews',
+                'title' => 'Platform Engineer, Mozilla',
+                'link' => '',
+                'img' => 'josh-matthews.png',
+                'desc' => "Josh Matthews is a software developer by trade, education, and passion. He works for Mozilla and has put blood, sweat, and tears into making Firefox a better web browser. Moreover, having spent a number of years as a volunteer for the project, he is fanatical about destroying barriers to entry for new open source contributors, and helping them succeed in whatever challenges they take on."
+            ),
+            array(
+                'name' => 'Maria Smirnoff',
+                'title' => 'Manager, TechU.Me/MaTechno.me',
+                'link' => '',
+                'img' => 'maria-smirnoff.png',
+                'desc' => "Maria has attended almost every CUSEC since 2007 as an exhibitor, promoting IBM’s unique, top-talent internship program Extreme Blue. During her time as the Extreme Blue Canada Program Manager, she had the honour and privilege of working with both technical and business student prodigies, most of whom now work at amazing technology companies like Google, IBM, Facebook, Amazon & Shopify (to name just a few). Maria thinks herself extremely fortunate to be able to call most of her former interns great friends. <br/>Nowadays, Maria is the Manager of Branding and Partnerships at TechU.me – an innovative program run by the Ottawa Network for Education that encourages high school students to explore STEM education and careers. The program’s flagship initiative is its Mentor Program, in which teams of high school and elementary school students work together to develop curriculum-based mobile apps with the help of industry mentors. A big part of Maria’s job is “working her network” (of afore-mentioned great friends) to find awesome mentors to participate in the program. <br/>Although not a techie by training (she holds a BComm and has just completed her MBA…gasp!), Maria is a geek at heart, having once co-founded a secret DDR society. Her most intimate experience with software engineering was an undergrad elective – Introduction to Object-Oriented Programming – in which she received an A-…in large part thanks to her Extreme Blue interns at the time."
+            ),
+            array(
+                'name' => 'Nadim Kobeissi',
+                'title' => 'Developer, Cryptocat',
+                'link' => '',
+                'img' => 'nadim-kobeissi.png',
+                'desc' => "Nadim Kobeissi founded Cryptocat, one of the world's most popular encrypted chat apps and an award-winning free and open source software project. His research focuses on making privacy and encryption technology more accessible and easy to use. Nadim is interested in web application engineering, and intersections between cryptography and foreign policy. He currently serves as a special advisor at the New America Foundation's Open Internet Tools Project. Nadim's work has been featured in the New York Times, Wired, TED, and other media."
+            ),
+            array(
+                'name' => 'Steve Sisalith',
+                'title' => 'Software Developer, SAP Labs',
+                'link' => '',
+                'img' => 'steve-sisalith.png',
+                'desc' => ""
+            ),
+            array(
+                'name' => 'Julia Evans',
+                'title' => 'Data Scientist & Co-Organizer of Montreal All-Girl Hack Night and PyLadies Montreal',
+                'link' => '',
+                'img' => 'julia-evans.png',
+                'desc' => "Julia Evans learned to program in math class on her TI-83 graphing calculator. Ten years and a couple of degrees later, she still enjoys programming a lot. She's worked as a web developer and data scientist. She enjoys playing with data and making interesting graphs, learning new programming languages, and learning that things that seem scary actually aren't. <br/>She cofounded Montreal All-Girl Hack Night and PyLadies Montréal, hangs out at Montréal Python a lot, and spent the fall of 2013 at Hacker School working on fun coding projects. Come talk to her about any of these things!"
+            ),
+            array(
+                'name' => 'Kelsey Gilmore-Innis',
+                'title' => 'Senior Backend Engineer, Reverb Technologies Inc.',
+                'link' => '',
+                'img' => '',
+                'desc' => ""
+            )
+        )
+    )
+);
+
+?>
