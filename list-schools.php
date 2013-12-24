a7687ca627dffc9b3f1c86efe243551c2681ec2d
Xenia Tay <xeniatzy@gmail.com>
2013-12-23 23:08:50 -0500

    Updated HDs

---
+++ list-schools.php
@@ -0,0 +1,79 @@
+<?php
+/* Pardon the hardcoded PHP.
+ * Guilty as charged. No excuses. :(
+ * Plz judge this book by its cover instead
+*/
+
+$schools = array(
+    array(
+        'name' => 'Carleton University',
+        'hd' => 'Kruti Bagde',
+        'link' => 'http://www.carleton.ca',
+        'email' => 'carleton@cusec.net',
+        'ga' => 'Carleton'
+    ),
+    array(
+        'name' => 'Concordia University',
+        'hd' => 'Patrick Nguyen',
+        'link' => 'http://www.concordia.ca',
+        'email' => 'concordia@cusec.net',
+        'ga' => 'Concordia'
+    ),
+    array(
+        'name' => 'McGill University',
+        'hd' => 'Sami Jaber',
+        'link' => 'http://www.mcgill.ca',
+        'email' => 'mcgill@cusec.net',
+        'ga' => 'McGill'
+    ),
+    array(
+        'name' => 'McMaster University',
+        'hd' => 'Saif Myageri',
+        'link' => 'http://www.mcmaster.ca',
+        'email' => 'mcmaster@cusec.net',
+        'ga' => 'McMaster'
+    ),
+    array(
+        'name' => "Queen's University",
+        'hd' => 'Jamie Bannerman',
+        'link' => 'http://www.queensu.ca/',
+        'email' => 'queens@cusec.net',
+        'ga' => 'Queens'
+    ),
+    array(
+        'name' => 'University of Guelph',
+        'hd' => 'Julian Horvat',
+        'link' => 'http://www.uoguelph.ca',
+        'email' => 'guelph@cusec.net',
+        'ga' => 'Guelph'
+    ),
+    array(
+        'name' => 'University of Ottawa',
+        'hd' => 'Stephanie Zeidan',
+        'link' => 'http://www.uottawa.ca',
+        'email' => 'ottawa@cusec.net',
+        'ga' => 'Ottawa'
+    ),
+    array(
+        'name' => 'University of Toronto Scarborough',
+        'hd' => 'David Kua',
+        'link' => 'http://www.utsc.utoronto.ca',
+        'email' => 'utsc@cusec.net',
+        'ga' => 'UTSC'
+    ),
+    array(
+        'name' => 'University of Waterloo',
+        'hd' => 'Alykhan Kanji',
+        'link' => 'http://www.uwaterloo.ca',
+        'email' => 'waterloo@cusec.net',
+        'ga' => 'Waterloo'
+    ),
+    array(
+        'name' => 'York University',
+        'hd' => 'Nataly Sheinin, Miguel Ocampo-Gooding',
+        'link' => 'http://www.yorku.ca',
+        'email' => 'york@cusec.net',
+        'ga' => 'York'
+    )
+);
+?>
--- page-schools.php
+++ page-schools.php
@@ -4,6 +4,8 @@ Template Name: Schools Page Template
 */
 ?>
 
+<?php include ('list-schools.php'); ?>
+
 <?php get_header(); ?>
 
     <div id='main-content' class='content page-content'>
@@ -24,136 +26,27 @@ Template Name: Schools Page Template
                 </div>
 
                 <ul class='schools-list clearfix'>
-                    <li>
-                        <div class='school-name'>
-                            <a href='http://www.carleton.ca' title='Carleton University'>
-                                Carleton University
-                            </a>
-                        </div>
-                        <div class='school-hds'>
-                            <a href="mailto:carleton@cusec.net" title="carleton@cusec.net" onclick="_gaq.push(['_trackEvent', 'mailto', 'HD - Carleton']);">
-                                Kruti Bagde
-                            </a>
-                        </div>
-                    </li><li>
-                        <div class='school-name'>
-                            <a href='http://www.concordia.ca' title='Concordia University'>
-                                Concordia University
-                            </a>
-                        </div>
-                        <div class='school-hds'>
-                            <a href="mailto:concordia@cusec.net" title="concordia@cusec.net" onclick="_gaq.push(['_trackEvent', 'mailto', 'HD - Concordia']);">
-                                Patrick Nguyen
-                            </a>
-                        </div>
-                    </li><li>
-                        <div class='school-name'>
-                            <a href='http://www.mcgill.ca' title='McGill University'>
-                                McGill University
-                            </a>
-                        </div>
-                        <div class='school-hds'>
-                            <a href="mailto:mcgill@cusec.net" title="mcgill@cusec.net" onclick="_gaq.push(['_trackEvent', 'mailto', 'HD - McGill']);">
-                                Sami Jaber
-                            </a>
-                        </div>
-                    </li><li>
-                        <div class='school-name'>
-                            <a href='http://www.mcmaster.ca' title='McMaster University'>
-                                McMaster University
-                            </a>
-                        </div>
-                        <div class='school-hds'>
-                            <a href="mailto:mcmaster@cusec.net" title="mcmaster@cusec.net" onclick="_gaq.push(['_trackEvent', 'mailto', 'HD - McMaster']);">
-                                Saif Myageri
-                            </a>
-                        </div>
-                    </li><li>
-                        <div class='school-name'>
-                            <a href='http://www.queensu.ca/' title="Queen's University">
-                                Queen's University
-                            </a>
-                        </div>
-                        <div class='school-hds'>
-                            <a href="mailto:queens@cusec.net" title="queens@cusec.net" onclick="_gaq.push(['_trackEvent', 'mailto', 'HD - Queens']);">
-                                Jamie Bannerman
-                            </a>
-                        </div>
-                    </li><li>
-                        <div class='school-name'>
-                            <a href='http://www.uoguelph.ca' title='University of Guelph'>
-                                University of Guelph
-                            </a>
-                        </div>
-                        <div class='school-hds'>
-                            <a href="mailto:guelph@cusec.net" title="guelph@cusec.net" onclick="_gaq.push(['_trackEvent', 'mailto', 'HD - Guelph']);">
-                                Julian Horvat
-                            </a>
-                        </div>
-                    </li><li>
-                        <div class='school-name'>
-                            <a href='http://www.uottawa.ca' title='University of Ottawa'>
-                                University of Ottawa
-                            </a>
-                        </div>
-                        <div class='school-hds'>
-                            <a href="mailto:ottawa@cusec.net" title="ottawa@cusec.net" onclick="_gaq.push(['_trackEvent', 'mailto', 'HD - Ottawa']);">
-                                Stephanie Zeidan
-                            </a>
-                        </div>
-                    </li><li>
-                        <div class='school-name'>
-                            <a href='http://www.utsc.utoronto.ca' title='University of Toronto Scarborough'>
-                                University of Toronto Scarborough
-                            </a>
-                        </div>
-                        <div class='school-hds'>
-                            <a href="mailto:utsc@cusec.net" title="utsc@cusec.net" onclick="_gaq.push(['_trackEvent', 'mailto', 'HD - UTSC']);">
-                                David Kua
-                            </a>
-                        </div>
-                    </li><li>
-                        <div class='school-name'>
-                            <a href='http://www.uwaterloo.ca' title='University of Waterloo'>
-                                University of Waterloo
-                            </a>
-                        </div>
-                        <div class='school-hds'>
-                            <a href="mailto:waterloo@cusec.net" title="waterloo@cusec.net" onclick="_gaq.push(['_trackEvent', 'mailto', 'HD - Waterloo']);">
-                                Alykhan Kanji
-                            </a>
-                        </div>
-                    </li><li>
-                        <div class='school-name'>
-                            <a href='http://www.wlu.ca/' title='Wilfrid Laurier University'>
-                                Wilfrid Laurier University
-                            </a>
-                        </div>
-                        <div class='school-hds'>
-                            <a href="mailto:laurier@cusec.net" title="laurier@cusec.net" onclick="_gaq.push(['_trackEvent', 'mailto', 'HD - WLU']);">
-                                Vaughan Hilts
-                            </a>
-                        </div>
-                    </li><li>
-                        <div class='school-name'>
-                            <a href='http://www.yorku.ca' title='York University'>
-                                York University
-                            </a>
-                        </div>
-                        <div class='school-hds'>
-                            <a href="mailto:york@cusec.net" title="york@cusec.net" onclick="_gaq.push(['_trackEvent', 'mailto', 'HD - York']);">
-                                Nataly Sheinin, Miguel Ocampo-Gooding
-                            </a>
-                        </div>
-                    </li>
+                    <?php foreach ($schools as $school): ?>
+                        <li>
+                            <div class='school-name'>
+                                <a href='<?php echo $school[link] ?>' title='<?php echo $school[name] ?>'>
+                                    <?php echo $school[name] ?>
+                                </a>
+                            </div>
+                            <div class='school-hds'>
+                                <a href="mailto:<?php echo $school[email] ?>" title="<?php echo $school[email] ?>" onclick="_gaq.push(['_trackEvent', 'mailto', 'HD - <?php echo $school[ga] ?>']);">
+                                    <?php echo $school[hd] ?>
+                                </a>
+                            </div>
+                        </li>
+                    <?php endforeach ?>
                 </ul>
 
                 <p class='page-cta'>
                     Want to be a Head Delegate for your school? Contact us at
                     <a class='page-cta-mailto' href="mailto:info@cusec.net" title="info@cusec.net" onclick="_gaq.push(['_trackEvent', 'mailto', 'info']);">
-                        info@cusec.net
+                        info@cusec.net.
                     </a>
-                    to be a head delegate.
                 </p>
 
             </div>
