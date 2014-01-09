c3091da0560a9f2aa24448352c2a1b8cbe4c3f84
Xenia Tay <xeniatzy@gmail.com>
2014-01-08 23:53:33 -0800

    Resume upload form

--- page-resume-upload.php
+++ page-resume-upload.php
@@ -12,6 +12,15 @@ Template Name: Resume Upload Page Template
                 <h2 class='section-heading light'>Resume Upload</h2>
                     <div class='page-content'>
                         <p class='page-cta'>Submit your resume to our sponsors before the career fair at CUSEC!</p>
+                        <p class='page-cta'>
+                            <strong>NEW: You can now include your submission for our Resume Roast!</strong>
+                        </p>
+                        <p class='resume-roast-cta'>
+                            Resume Roast is a lunchtime workshop that will feature a panel of experienced tech-industry judges going through common blunders in example resumes.
+                        </p>
+                        <p class='page-cta cta-note'>
+                            Note: If you previously uploaded your resume for companies only, add your resume for the Roast by submitting this form with the 'Companies' field unchecked.
+                        </p>
                         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
                             <?php the_content(); ?>
                         <?php endwhile; endif; ?>
