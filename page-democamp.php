7dedeaad0d28298c9837358668f2280819e75dbd
Xenia Tay <xeniatzy@gmail.com>
2014-01-20 00:02:00 -0800

    Or not, looks weird

--- page-democamp.php
+++ page-democamp.php
@@ -32,7 +32,7 @@ Template Name: Democamp Page Template
                 </div>
 
                 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-                    <?php // the_content(); ?>
+                    <?php the_content(); ?>
                 <?php endwhile; endif; ?>
             </div>
         </div>
