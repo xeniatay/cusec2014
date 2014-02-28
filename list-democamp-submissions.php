57e708b9c6b464ce2a2c9a6a93290ba172f266ad
Xenia Tay <xeniatzy@gmail.com>
2014-02-27 20:02:56 -0800

    Democamp submissions

--- list-democamp-submissions.php
+++ list-democamp-submissions.php
@@ -4,30 +4,45 @@
  * Plz judge this book by its cover instead
 */
 
-$democamp_submissions = array(
+$democamp_winners = array(
     array(
-        'team' => 'Martin Gingras - Carleton University',
-        'project_name' => 'FBomb.co',
-        'project_desc' => 'See where in the world the fBomb was dropped. This is an interactive map that shows where people are using the F-word on Twitter. The app is written in CoffeeScript and uses the express framework for Node.js. It hooks into Twitter\'s streaming api, particularly the POST statuses/filter and uses bootstrap, JQuery, and Google Maps on the front end. Since being published in late October last year, the app has received international press coverage, 3K+ shares on Twitter, 9K+ shares on Facebook, and over 250K unique visitors from around the world. <br/>There is a full write up at http://mgingras.ca/coding/fBomb/.',
-        'explanation' => 'My inspiration for this application actually came from a discussion I had with two colleagues of mine while doing co-op at BlackBerry, Ryan Seys and Elliott Ro. We were talking about how people misuse and abuse the english language. As the conversation devolved swearing was brought up. In particular we talked about how swearing can completely undermine ones argument. Earlier that day we had been talking about the personal projects we were working and, somehow at this point we circled back to that topic. From there our conversation wound it\'s way to how it\'d be neat to map where people swear. Within a few minutes and iterations of ideas the concept for the app was born. <br/>Project: <a href="http://www.fbomb.co">http://www.fbomb.co</a> - Code: <a href="http://www.github.com/mgingras/fBomb">http://github.com/mgingras/fBomb</a>'
+        'team' => 'Mack Duan, David Hu, Jamie Wong, Sandy Wu, all from the University of Waterloo',
+        'project_name' => 'UW Flow',
+        'project_desc' => 'Flow is a website designed to help students at the University of Waterloo plan their university courses. The website offers descriptions, schedules, ratings, and reviews for courses offered at the university. It also allows students to see what courses their friends are taking, what they\'ve taken, and their thoughts on those courses. To date, 4400 students have signed up for the product, along with others who use the website without an account.',
+        'explanation' => 'We met each other through being classmates over the past 4 years. Over a year ago, we decided to work on a project together and brainstormed hundreds of ideas about what to build. We ended up continuing and expanding on a side project that one of the team members, Mack Duan, had already been working on. His project tackled the problem of trying to make informed course decisions when we had to select courses each term. <a href="http://uwflow.com/">http://uwflow.com/</a>'
     ),
     array(
-        'team' => 'Yann Landry, Université de Moncton',
-        'project_name' => 'Textchange',
-        'project_desc' => 'Tired of being robbed at the bookstore? Use Textchange to advertise, sell, exchange your used textbooks in a convenient, clear and organized way. http://textchange.ca',
-        'explanation' => 'The base concept already exists and is pretty simple. There was a need for such a platform in my region so I went forward with the idea. Textchange was builded during the summer of 2013 after my work hours.'
-    ),
-    array(
-        'team' => 'Brandon Byskov - McMaster University',
-        'project_name' => 'Research on Intelligent Machines',
-        'project_desc' => 'I\'m doing research on Intelligent Machines. I\'m looking at the math behind how they would operate and the abstract models on which they could be based. I\'m also exploring the how to design such a model of intelligence and considerations around various aspects of the design and implementation of intelligent machines. I’ve been incorporating many diverse ideas into my work, from my own investigative research, to the mathematical and philosophical works of Aristotle and George Boole, and to the modern ideas in artificial intelligence from the last 60 years. <br/> I’m working on this project independently. I\'m writing a series of papers on this topic. The first paper in the series is close to completion, and will be a 30-page broad introduction to the fundamental theory behind this work and to serve as an intuitive basis from which to understand later works in the series.',
-        'explanation' => 'I was inspired by some lectures that we're given by Stanford professor Andrew Ng, where he discussed deep learning algorithms and the brain. Since over one year ago, I haven't been able to stop thinking about this problem of how to build an intelligent machine. Ray Kurzweil, Director of Engineering at Google, believes that we’re very close to building this type of technology, and that's the goal I'm working towards.'
+        'team' => 'Peter Sobot - Waterloo',
+        'project_name' => 'The Wub Machine',
+        'project_desc' => 'Remix your favourite music into Dubstep, Swing, or any other popular genre at the click of a button. Over 1,000,000 remixes served! Available on the web, iOS or Android.',
+        'explanation' => 'The idea came out of a hack day at Waterloo (SE Hack Day). Live and free at <a href="http://the.wubmachine.com">http://the.wubmachine.com</a>.'
     ),
     array(
         'team' => 'Rohan Likhite and Ursula Sarracini from York University',
         'project_name' => 'Homunculus Series AR',
         'project_desc' => 'This is an experiment designed for the IOS/Android platform. It features augmented 3D models which extend the physical installation titled Homunculus Agora by Dr. Mark-David Hosale. This application along side the physical installation was displayed in the Markham Museum for the LandSlide Art Exhibition. This application is a virtual representation launched when the user scans a designated target with their device. <br/>Through the app, visitors of the gallery are immersed within an augmented reality experience that provides an alternate view and virtual flythrough of the piece. In addition, series of markers located in the exhibition connect to virtual information panels that describe various aspects and details of the work. <br/>This application was showcased alongside Dr. Hosale\'s piece in the following countries: <br/>1) Canada (LandSlide Future Possibilities Exhibit - Ontario)<br/>2) China (Shenzhen Bi-City Biennale of Urbanism/Architecture - Hong Kong)<br/>3) Denmark (renew Digital Arts Forum - Copenhagen)<br/>It is currently available on the Google Play Store, and is in the process of being accepted to the Apple AppStore.',
         'explanation' => 'We were recruited by the artist who required developers to design a mobile application that would accompany his piece in the Markham Museum. Dr. Mark-David Hosale was also interested in taking his piece to several different exhibitions internationally, and wanted a standalone application which would provide users with information on the piece as well as extend the virtual experience. <br/>We met with the artist and brainstormed ideas for an augmented reality concept which was later on developed and exhibited in the museum. Due to the large physical size of the piece, transporting it internationally was not feasible, therefore this application would be useful for those who wanted to see the piece in it\'s entirety. Inspiration was drawn from the idea of virtual spaces, and the idea of experiencing physical objects without being physically present in the space. <br/>Here is a link to the Google Play store where Homunculus Series AR can be downloaded: <a href="http://bit.ly/1d1rzvq">http://bit.ly/1d1rzvq</a> <br/>**It is currently in the process of being accepted to the Apple AppStore.'
+    ),
+);
+
+$democamp_submissions = array(
+    array(
+        'team' => 'Martin Gingras - Carleton University',
+        'project_name' => 'FBomb.co',
+        'project_desc' => 'See where in the world the fBomb was dropped. This is an interactive map that shows where people are using the F-word on Twitter. The app is written in CoffeeScript and uses the express framework for Node.js. It hooks into Twitter\'s streaming api, particularly the POST statuses/filter and uses bootstrap, JQuery, and Google Maps on the front end. Since being published in late October last year, the app has received international press coverage, 3K+ shares on Twitter, 9K+ shares on Facebook, and over 250K unique visitors from around the world. <br/>There is a full write up at <a href="http://mgingras.ca/coding/fBomb/">.http://mgingras.ca/coding/fBomb/</a>',
+        'explanation' => 'My inspiration for this application actually came from a discussion I had with two colleagues of mine while doing co-op at BlackBerry, Ryan Seys and Elliott Ro. We were talking about how people misuse and abuse the english language. As the conversation devolved swearing was brought up. In particular we talked about how swearing can completely undermine ones argument. Earlier that day we had been talking about the personal projects we were working and, somehow at this point we circled back to that topic. From there our conversation wound it\'s way to how it\'d be neat to map where people swear. Within a few minutes and iterations of ideas the concept for the app was born. <br/>Project: <a href="http://www.fbomb.co">http://www.fbomb.co</a> - Code: <a href="http://www.github.com/mgingras/fBomb">http://github.com/mgingras/fBomb</a>'
+    ),
+    array(
+        'team' => 'Yann Landry, Université de Moncton',
+        'project_name' => 'Textchange',
+        'project_desc' => 'Tired of being robbed at the bookstore? Use Textchange to advertise, sell, exchange your used textbooks in a convenient, clear and organized way. <a href="http://textchange.ca">http://textchange.ca</a>',
+        'explanation' => 'The base concept already exists and is pretty simple. There was a need for such a platform in my region so I went forward with the idea. Textchange was builded during the summer of 2013 after my work hours.'
+    ),
+    array(
+        'team' => 'Brandon Byskov - McMaster University',
+        'project_name' => 'Research on Intelligent Machines',
+        'project_desc' => 'I\'m doing research on Intelligent Machines. I\'m looking at the math behind how they would operate and the abstract models on which they could be based. I\'m also exploring the how to design such a model of intelligence and considerations around various aspects of the design and implementation of intelligent machines. I’ve been incorporating many diverse ideas into my work, from my own investigative research, to the mathematical and philosophical works of Aristotle and George Boole, and to the modern ideas in artificial intelligence from the last 60 years. <br/> I’m working on this project independently. I\'m writing a series of papers on this topic. The first paper in the series is close to completion, and will be a 30-page broad introduction to the fundamental theory behind this work and to serve as an intuitive basis from which to understand later works in the series.',
+        'explanation' => 'I was inspired by some lectures that we\'re given by Stanford professor Andrew Ng, where he discussed deep learning algorithms and the brain. Since over one year ago, I haven\'t been able to stop thinking about this problem of how to build an intelligent machine. Ray Kurzweil, Director of Engineering at Google, believes that we’re very close to building this type of technology, and that\'s the goal I\'m working towards.'
     ),
     array(
         'team' => 'Spencer Kelly (Queens)',
@@ -44,7 +59,7 @@ $democamp_submissions = array(
     array(
         'team' => 'Ryan Seys - Carleton University',
         'project_name' => 'NodeJS + Jawbone UP',
-        'project_desc' => 'I wrote a Jawbone UP API Library for NodeJS (npmjs.org/jawbone-up) <br/>I also created a 'steps' app which uses the above library to get total steps ever walked with UP. I hope to expand this app into something everyday users can use to gauge progress and work towards a much bigger goal than just a daily '10000' steps. <br/>Looking for additional ideas to build out apps for the Jawbone UP platform and get people motivated to becoming more active.',
+        'project_desc' => 'I wrote a Jawbone UP API Library for NodeJS (npmjs.org/jawbone-up) <br/>I also created a \'steps\' app which uses the above library to get total steps ever walked with UP. I hope to expand this app into something everyday users can use to gauge progress and work towards a much bigger goal than just a daily \'10000\' steps. <br/>Looking for additional ideas to build out apps for the Jawbone UP platform and get people motivated to becoming more active.',
         'explanation' => 'I use a Jawbone UP daily to track sleep and movement habits and I noticed a lack of applications built for the platform, despite a lovely API given and supported by Jawbone. I hope to help kick-start some development on the platform to promote healthier living and to better understand my habits. <br/>node-jawbone-up: <a href="http://github.com/ryanseys/node-jawbone-up">http://github.com/ryanseys/node-jawbone-up</a><br/>steps: <a href="http://github.com/ryanseys/steps">http://github.com/ryanseys/steps</a>'
     ),
     array(
@@ -52,24 +67,6 @@ $democamp_submissions = array(
         'project_name' => 'estimatemymiles.com',
         'project_desc' => 'estimatemymiles.com is a free tool that allows frequent flyers to make informed decisions about which flight to take in order to maximise the mileage accrued. Whether you\'re saving miles for a round-the-world trip, or trying to meet an elite qualification threshold, or simply want to optimise your flight plans to earn more miles, estimatemymiles.com can help you get there. <br/>Built using Django and AngularJS. Source on Github: <a href="http://github.com/dellsystem/estimatemymiles.com">http://github.com/dellsystem/estimatemymiles.com</a>',
         'explanation' => 'I built it as a result of how difficult it is to find mileage accrual information on official airline websites. <br/>Link: <a href="http://estimatemymiles.com">http://estimatemymiles.com</a>'
-    ),
-    array(
-        'team' => '',
-        'project_name' => '',
-        'project_desc' => '',
-        'explanation' => ''
-    ),
-    array(
-        'team' => 'Mack Duan, David Hu, Jamie Wong, Sandy Wu, all from the University of Waterloo',
-        'project_name' => 'UW Flow',
-        'project_desc' => 'Flow is a website designed to help students at the University of Waterloo plan their university courses. The website offers descriptions, schedules, ratings, and reviews for courses offered at the university. It also allows students to see what courses their friends are taking, what they\'ve taken, and their thoughts on those courses. To date, 4400 students have signed up for the product, along with others who use the website without an account.',
-        'explanation' => 'We met each other through being classmates over the past 4 years. Over a year ago, we decided to work on a project together and brainstormed hundreds of ideas about what to build. We ended up continuing and expanding on a side project that one of the team members, Mack Duan, had already been working on. His project tackled the problem of trying to make informed course decisions when we had to select courses each term. <a href="http://uwflow.com/">http://uwflow.com/</a>'
-    ),
-    array(
-        'team' => 'Peter Sobot - Waterloo',
-        'project_name' => 'The Wub Machine',
-        'project_desc' => 'Remix your favourite music into Dubstep, Swing, or any other popular genre at the click of a button. Over 1,000,000 remixes served! Available on the web, iOS or Android.',
-        'explanation' => 'The idea came out of a hack day at Waterloo (SE Hack Day). Live and free at <a href="http://the.wubmachine.com">http://the.wubmachine.com</a>.'
     ),
     array(
         'team' => 'Thusanthan Kannan, Kulwinder Billen, Jerry Oku) - McMaster',
@@ -93,7 +90,7 @@ $democamp_submissions = array(
         'team' => 'Michael Chang, University of Waterloo / Louisa Chong, University of Waterloo',
         'project_name' => 'ideas.iterate.ca',
         'project_desc' => 'A tool to tell student entrepreneurs if their start-up/project idea is "obviously bad" (i.e. falls into an industry or market which is known to be difficult for students to build a start-up/project in), and why it is "obviously bad".',
-        'explanation' => 'I came up with the idea while observing students participating in the Winter 2014 offering of VeloCity bootcamp for an independent study project. I met my team members during the same offering of VeloCity bootcamp. <br/>Live Demo: <a href="http://ideas.iterate.ca/">http://ideas.iterate.ca/ <br/>Source Code: <a href="https://github.com/cbhl/ideas.iterate.ca">https://github.com/cbhl/ideas.iterate.ca</a>'
+        'explanation' => 'I came up with the idea while observing students participating in the Winter 2014 offering of VeloCity bootcamp for an independent study project. I met my team members during the same offering of VeloCity bootcamp. <br/>Live Demo: <a href="http://ideas.iterate.ca/">http://ideas.iterate.ca/</a> <br/>Source Code: <a href="https://github.com/cbhl/ideas.iterate.ca">https://github.com/cbhl/ideas.iterate.ca</a>'
     )
 );
 ?>
--- page-democamp.php
+++ page-democamp.php
@@ -4,11 +4,67 @@ Template Name: Democamp Page Template
 */
 ?>
 
+<?php include ('list-democamp-submissions.php'); ?>
+
 <?php get_header(); ?>
 
     <div id='main-content' class='content page-content'>
         <div class='page-democamp'>
             <div class='container'>
+                <h2 class='section-heading light'>Democamp Presentations</h2>
+                <p class='page-cta'><strong>DemoCamp is an opportunity to demo and present a project that you or a team have been working on.</strong></p>
+                <p class='page-cta'><strong>We would like to congratulate the top 3 winners of Democamp!</strong></p>
+                <ol>
+                    <?php foreach ($democamp_winners as $proj): ?>
+                        <li class='democamp-submission'>
+                            <p class='democamp-submission-name democamp-attr'>
+                                <span class='democamp-label'>Project:</span>
+                                <? echo $proj[project_name] ?>
+                            </p>
+                            <p class='democamp-team democamp-attr'>
+                                <span class='democamp-label'>Team:</span>
+                                <? echo $proj[team] ?>
+                            </p>
+                            <p class='democamp-submission-desc democamp-attr'>
+                                <span class='democamp-label-block'>Description:</span>
+                                <? echo $proj[project_desc] ?>
+                            </p>
+                            <p class='democamp-submission-explanation democamp-attr'>
+                                <span class='democamp-label-block'>Project inspiration, founding story, and relevant links:</span>
+                                <? echo $proj[explanation] ?>
+                            </p>
+                        </li>
+                        <hr/>
+                    <? endforeach ?>
+                </ol>
+
+                <p class='page-cta'><strong>Thank you to all of our other Democamp presenters. We wish you the best of luck with your projects!</strong></p>
+                <p class='page-cta'>Below are the list of presentations, in no particular order.</p>
+                <hr/>
+                <ol>
+                    <?php foreach ($democamp_submissions as $proj): ?>
+                        <li class='democamp-submission'>
+                            <p class='democamp-submission-name democamp-attr'>
+                                <span class='democamp-label'>Project:</span>
+                                <? echo $proj[project_name] ?>
+                            </p>
+                            <p class='democamp-team democamp-attr'>
+                                <span class='democamp-label'>Team:</span>
+                                <? echo $proj[team] ?>
+                            </p>
+                            <p class='democamp-submission-desc democamp-attr'>
+                                <span class='democamp-label-block'>Description:</span>
+                                <? echo $proj[project_desc] ?>
+                            </p>
+                            <p class='democamp-submission-explanation democamp-attr'>
+                                <span class='democamp-label-block'>Project inspiration, founding story, and relevant links:</span>
+                                <? echo $proj[explanation] ?>
+                            </p>
+                        </li>
+                        <hr/>
+                    <? endforeach ?>
+                </ol>
+
                 <h2 class='section-heading light'>Democamp sponsored by <a href="http://velocity.uwaterloo.ca" title="Velocity">Velocity</a></h2>
                 <div class='democamp-desc'>
                     <p><strong>DemoCamp is an opportunity to demo and present a project that you or a team have been working on.</strong></p>
@@ -32,8 +88,7 @@ Template Name: Democamp Page Template
                 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
                     <?php the_content(); ?>
                 <?php endwhile; endif; ?>
-                <p class='page-cta'>Democamp submissions are now closed! Be sure to catch the event on Thursday, January 16th from 7pm - 8pm!</p>
-
+                <p class='page-cta'>Thank you for your interest in Democamp, submissions are currently closed.</p>
 
             </div>
         </div>
